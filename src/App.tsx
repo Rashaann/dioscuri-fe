e2400d2c8390467d418b3f0e4cf847f406b7555e
Rashaann <rashaandesnoyers@gmail.com>
2025-07-14 20:11:38 +0200

    feat: Choose game logic added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import './App.css'
-import Game from './components/Game'
+import { useState } from "react";
+import "./App.css";
+import Game from "./components/Game";
+import Intro from "./components/Intro";
+import Game2 from "./components/Game2";
 
 function App() {
-
+  const [whichGame, setWhichGame] = useState("");
   return (
-    <div style={{width: "100vw", height: "100vh", padding: 0, backgroundColor: 'red'}}>
-        <Game />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      {!whichGame && <Intro setWhichGame={setWhichGame} />}
+      {whichGame !== "" && (whichGame === "game1" ? <Game /> : <Game2 />)}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
