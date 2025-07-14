57610acd8c5ab3135bef4a5be47f31667e6c0e2f
Rashaann <rashaandesnoyers@gmail.com>
2025-07-14 20:11:19 +0200

    feat: Intro games component created

---
+++ src/components/Intro.tsx
@@ -0,0 +1,34 @@
+import type { Dispatch, SetStateAction } from "react";
+
+const Intro = (props: { setWhichGame: Dispatch<SetStateAction<string>> }) => {
+  const { setWhichGame } = props;
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+        gap: 20,
+      }}
+    >
+      Which game to choose?
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          width: "100%",
+          gap: 30,
+        }}
+      >
+        <button onClick={() => setWhichGame("game1")}>game 1</button>
+        <button onClick={() => setWhichGame("game2")}>game 2</button>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
